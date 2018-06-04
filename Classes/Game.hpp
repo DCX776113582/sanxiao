f8d9414b66d5f391b14295777908ada2f6aac8a8
DCX776113582 <776113582@qq.com>
2018-06-04 16:56:37 +0800

    修改2

--- Classes/Game.hpp
+++ Classes/Game.hpp
@@ -36,24 +36,35 @@ USING_NS_CC;
 class Game :public Layer
 {
 public:
-    static Scene* createScene();
-    bool init()override;
-    CREATE_FUNC(Game);
+    static Layer* create(int chapter,int Step);
+    static Scene* createScene(int chapter,int Step);
+    bool init(int chapter,int Step);
+//    CREATE_FUNC(Game);
     bool jiancha();
     void buchong();
     void onEnter()override;
     void onExit()override;
     void fun_shanchu();
     void fun_shanchu1();
-//    void win();
-//    void lose();
+
+    void win();
+    void lose();
+    void updatetime(float dt);
+    void updateStep(float dt);
+    void parseJSon();//解析文件——修改
     void chushihua();//初始化
+    void menu();
+    
     bool onTouchBegan(Touch * touch,Event * unused_event)override;
     void onTouchMoved(Touch * touch,Event * unused_event)override;
     void onTouchEnded(Touch * touch,Event * unused_event)override;
     void onTouchCancelled(Touch * touch,Event * unused_event)override;
 //    int tianjiakuai();
 private:
+    int _chapter,_Step;
+    int _highscore;
+    int HighScore;
+    int TargetScore;
     bool xiugai;
     Baoshi * jihe[8][8];
     Vector<Baoshi*> shanchu;
@@ -62,8 +73,17 @@ private:
     Sprite * kuang;
     Baoshi * jiaohuan;
     int tag;
-    Label * score;
+    Size virableSize;
+    Text * score;
+    Text * score1;
+    Text * label;
+    Text * hinghscore;
+    Node * node1;
+    Node* node2;
+    int _step;
+    Text * step;
     int sc;
+    int num,num2;
 };
 
 #endif /* Game_hpp */
